8c2b8bfcabbc598eff6a15d5f7539766e133a7f0
Magesh08 <mageshvaran16@gmail.com>
2022-06-13 23:27:02 +0530

    added date formate

--- src/components/Expanseitem.jsx
+++ src/components/Expanseitem.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import './Expanseitem.css';
 
 function Expanseitem(props) {
+    
+    const mounth= props.date.toLocaleString('en-US',{mounth: 'long'});
+    const day=props.date.toLocaleString('en-US',{day: '2-digit'});
+    const year= props.date.getFullYear();
+
     return(
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{mounth}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
